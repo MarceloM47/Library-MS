7bd0788b32026613e774b201c31c3da33fa616c9
MarceloM47 <morinigomarcelo40@gmail.com>
2023-09-08 10:51:52 -0400

    Solves the problem of not allowed parameters of the devise gem and creates the views for the registry

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  root "specialities#index"
   resources :specialities
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'user/registrations',
+    sessions: 'user/sessions'
+  }
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
