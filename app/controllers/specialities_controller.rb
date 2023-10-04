0190a8e9d09cddba1c26ae76aa9ed05071104c1b
MarceloM47 <morinigomarcelo40@gmail.com>
2023-10-04 14:28:56 -0300

    The translation of the specialties controller was made

--- app/controllers/specialities_controller.rb
+++ app/controllers/specialities_controller.rb
@@ -29,7 +29,7 @@ class SpecialitiesController < ApplicationController
 
     respond_to do |format|
       if @speciality.save
-        format.html { redirect_to speciality_url(@speciality), notice: "Speciality was successfully created." }
+        format.html { redirect_to speciality_url(@speciality), notice: "La especialidad ha sido creado con éxito." }
         format.json { render :show, status: :created, location: @speciality }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class SpecialitiesController < ApplicationController
   def update
     respond_to do |format|
       if @speciality.update(speciality_params)
-        format.html { redirect_to speciality_url(@speciality), notice: "Speciality was successfully updated." }
+        format.html { redirect_to speciality_url(@speciality), notice: "La especialidad ha sido actualizada con éxito." }
         format.json { render :show, status: :ok, location: @speciality }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class SpecialitiesController < ApplicationController
     @speciality.destroy
 
     respond_to do |format|
-      format.html { redirect_to specialities_url, notice: "Speciality was successfully destroyed." }
+      format.html { redirect_to specialities_url, notice: "La especialidad ha sido eliminada con éxito." }
       format.json { head :no_content }
     end
   end
